5bf2054503f6b4368066c6e2e38bfe873f49a0ad
Nemesis357 <nenadn89@gmail.com>
2018-01-10 20:10:59 +0100

    commit

--- src/main/java/ninja/nenad/projectninja/config/WebMvcConfig.java
+++ src/main/java/ninja/nenad/projectninja/config/WebMvcConfig.java
@@ -226,7 +226,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	 */
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
 	}
 	
 	@Override
